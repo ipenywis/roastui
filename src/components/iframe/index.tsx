e799a0919dbb0319bb365818fbb19d15dcb3b663
Islem Maboud <islem@goperigon.com>
2024-11-29 10:05:06 +0100

    fix: custom iframe element with suspense not properly loading

--- src/components/iframe/index.tsx
+++ src/components/iframe/index.tsx
@@ -1,4 +1,11 @@
-import { Suspense, useLayoutEffect, useRef, useState } from 'react';
+import {
+  Suspense,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 
 type IFrameProps = React.ComponentPropsWithRef<'iframe'> & {
   fallback?: JSX.Element;
@@ -6,8 +13,6 @@ type IFrameProps = React.ComponentPropsWithRef<'iframe'> & {
 
 export function IFrame(props: IFrameProps) {
   const { fallback, ...rest } = props;
-
-  return <iframe {...rest} />;
 
   return (
     <Suspense fallback={fallback || 'loading...'}>
@@ -17,15 +22,20 @@ export function IFrame(props: IFrameProps) {
 }
 
 function IFrameImplementation(props: React.ComponentPropsWithRef<'iframe'>) {
+  const { src, onLoad: onLoadProp, onError: onErrorProp } = props;
+
   const awaiter = useRef<null | {
     promise: null | Promise<void>;
     resolve: () => void;
     reject: () => void;
   }>(null);
   const [_, triggerLoad] = useState(false);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
   if (awaiter.current?.promise) {
     throw awaiter.current.promise;
   }
+
   useLayoutEffect(() => {
     if (awaiter.current === null) {
       // @ts-ignore
@@ -38,25 +48,44 @@ function IFrameImplementation(props: React.ComponentPropsWithRef<'iframe'>) {
       triggerLoad(true);
     }
   }, []);
+
   const { title } = props;
-  return (
-    <iframe
-      {...props}
-      title={title}
-      onLoad={(e) => {
-        // @ts-ignore
-        awaiter.current.promise = null;
-        awaiter.current?.resolve();
-        props.onLoad?.(e);
-      }}
-      onError={(err) => {
-        // @ts-ignore
-        awaiter.current.promise = null;
-        awaiter.current?.reject();
-        props.onError?.(err);
-      }}
-    />
+
+  const onLoad = useCallback(
+    (e: Event) => {
+      // @ts-ignore
+      awaiter.current.promise = null;
+      awaiter.current?.resolve();
+      onLoadProp?.(e as any);
+    },
+    [onLoadProp]
   );
+
+  const onError = useCallback(
+    (e: Event) => {
+      // @ts-ignore
+      awaiter.current.promise = null;
+      awaiter.current?.reject();
+      onErrorProp?.(e as any);
+    },
+    [onErrorProp]
+  );
+
+  useEffect(() => {
+    const currentIframeRef = iframeRef.current;
+    if (currentIframeRef && src) {
+      currentIframeRef.src = src;
+    }
+    currentIframeRef?.addEventListener('load', onLoad);
+    currentIframeRef?.addEventListener('error', onError);
+
+    return () => {
+      currentIframeRef?.removeEventListener('load', onLoad);
+      currentIframeRef?.removeEventListener('error', onError);
+    };
+  }, [onLoad, onError, src]);
+
+  return <iframe {...props} ref={iframeRef} title={title} />;
 }
 
 export default IFrame;
