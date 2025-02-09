1508b232c87ad247d1c314eb2e51e31bbaf670f8
Islem Maboud <islem@goperigon.com>
2025-02-09 20:00:44 +0100

    fix: billing exception

--- src/app/(main)/billing/page.tsx
+++ src/app/(main)/billing/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from '@/auth';
+import { prisma } from '@/lib/prisma';
 import stripe from '@/lib/stripe';
 import { redirect } from 'next/navigation';
 
@@ -9,24 +10,34 @@ export default async function BillingPage() {
     redirect('/login');
   }
 
-  const { data: customers } = await stripe.customers.list({
-    email: session.user.email,
-    limit: 1,
-  });
-
   let redirectUrl = '';
 
   try {
-    const billingPortalSession = await stripe.billingPortal.sessions.create({
-      customer: customers[0].id,
-      return_url: `${process.env.APP_URL}`,
+    const subscription = await prisma.subscription.findFirst({
+      where: {
+        userId: session.user.id,
+      },
     });
 
-    if (!billingPortalSession.url) {
-      return <div>Something went wrong! Please try again.</div>;
-    }
+    if (!subscription || !subscription?.isActive) {
+      redirectUrl = '/pricing';
+    } else {
+      const { data: customers } = await stripe.customers.list({
+        email: session.user.email,
+        limit: 1,
+      });
 
-    redirectUrl = billingPortalSession.url;
+      const billingPortalSession = await stripe.billingPortal.sessions.create({
+        customer: customers[0].id,
+        return_url: `${process.env.APP_URL}`,
+      });
+
+      if (!billingPortalSession.url) {
+        return <div>Something went wrong! Please try again.</div>;
+      }
+
+      redirectUrl = billingPortalSession.url;
+    }
   } catch (err) {
     //eslint-disable-next-line no-console
     console.error('Error:', err);
